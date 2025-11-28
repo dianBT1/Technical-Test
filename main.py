214e20c440d0f0b825481fbbdc544bdfd61ddf3d
Dian Alhusari <alhusari2@gmail.com>
2025-11-28 23:00:15 +0700

    update return

--- main.py
+++ main.py
@@ -8,19 +8,11 @@ app = FastAPI(title="Learning RAG Demo")
 
 @app.post("/ask")
 def ask_question(req: QuestionRequest):
-    start = time.time()
     try:
         result = workflowcontroller.rag_workflow(req.question)
-        return {
-            "question": req.question,
-            "answer": result["answer"],
-            "context_used": result.get("context", []),
-            "latency_sec": round(time.time() - start, 3)
-        }
+        return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
 @app.post("/add")
 def add_document(req: DocumentRequest):
     try:
